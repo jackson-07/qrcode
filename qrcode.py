f638b79bb65e1b210838c0ebe5239a642ffc2eec
Jackson Raymond <84482662+jackson-07@users.noreply.github.com>
2021-12-23 12:25:14 +1100

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -3,6 +3,6 @@ import os, shutil
 
 file_name_png = "linkedin" + ".png"
 
-url = pyqrcode.create("https://www.linkedin.com/in/jackson-raymond-376570104/")
+url = pyqrcode.create("https://www.linkedin.com/in/jackson-raymond07/")
 
 url.png(file_name_png, scale=8)
