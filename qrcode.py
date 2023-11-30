6dd485d6cf7854ef8d136a0589e4c7cb7d1636de
Jackson Raymond <84482662+jackson-07@users.noreply.github.com>
2023-12-01 09:35:25 +1100

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -3,6 +3,6 @@ import os, shutil
 
 file_name_png = "qrcode" + ".png"
 
-url = pyqrcode.create("https://jackson-07.github.io/blog/")
+url = pyqrcode.create("https://jackson-07.github.io/website/")
 
 url.png(file_name_png, scale=8)
